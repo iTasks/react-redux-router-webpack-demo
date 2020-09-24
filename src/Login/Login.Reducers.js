333ae795b4f0724dc0b63d13e5c103092c8b0617
Kunkka Li <liqili2005@gmail.com>
2020-09-24 22:35:52 +1200

    format

--- src/Login/Login.Reducers.js
+++ src/Login/Login.Reducers.js
@@ -1,5 +1,3 @@
-
-
 import TYPES from './Login.Constants';
 
 const initialState = {
@@ -11,38 +9,37 @@ const initialState = {
 export default function reducer(state = initialState, action) {
 
     switch (action.type) {
-    case TYPES.LOGGED_DOING:
-        return {
-            ...state,
-            status: 'doing'
-        };
+        case TYPES.LOGGED_DOING:
+            return {
+                ...state,
+                status: 'doing'
+            };
 
-    case TYPES.LOGGED_IN:
-        return {
-            ...state,
-            isLoggedIn: true,
-            user: action.user,
-            status: 'done'
-        };
+        case TYPES.LOGGED_IN:
+            return {
+                ...state,
+                isLoggedIn: true,
+                user: action.user,
+                status: 'done'
+            };
 
-    case TYPES.LOGGED_OUT:
-        console.log("Log out successuflly=>>>>>>>>>>")
-        return {
-            ...state,
-            isLoggedIn: false,
-            user: {},
-            status: null
-        };
-    case TYPES.LOGGED_ERROR:
-        return {
-            ...state,
-            isLoggedIn: false,
-            user: {},
-            status: null
-        }
+        case TYPES.LOGGED_OUT:
+            return {
+                ...state,
+                isLoggedIn: false,
+                user: {},
+                status: null
+            };
+        case TYPES.LOGGED_ERROR:
+            return {
+                ...state,
+                isLoggedIn: false,
+                user: {},
+                status: null
+            }
 
-    default:
-        return state;
+        default:
+            return state;
     }
 
 }
